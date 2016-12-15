7fabc175c4393514bce3e0889581850f75d9edda
Usuario <Usuario@192.168.30.250>
2016-12-15 10:49:13 +0100

    jar arreglado

--- src/Models/BD.java
+++ src/Models/BD.java
@@ -7,8 +7,8 @@ public class BD {
     private String basededatos = "JGZ.db4o";
 
     public void Prueba() {
-        ObjectContainer bd = Db4o.openFile(Db4o.newConfiguration(), basededatos);
-        //Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), basededatos);
+        ObjectContainer bd = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), basededatos);
+
         try {
             //metodos sobre la BD
 
