e2e40746375b12ee20743cd6833db108d8e3f961
Pablo Verdugo Huerta <pablo.verdugo000@gmail.com>
2017-03-02 16:57:18 +0100

    Insertar/modificar/borrar canciones
    Falta testeo.

--- src/Controllers/Controller.java
+++ src/Controllers/Controller.java
@@ -6,7 +6,8 @@ import Models.Cantante;
 import Views.MainView;
 import Models.Consultas;
 import com.db4o.ObjectContainer;
-import javax.swing.JDialog;
+
+import javax.swing.*;
 import java.util.ArrayList;
 
 //http://panamahitek.com/crear-un-archivo-jar-con-librerias-externas/
@@ -28,8 +29,20 @@ public class Controller {
 
     void iniciar() {
         Cantante cantaor = new Cantante("manuel", "Flamenco");
-        consulta.insertCanciones(bd, "nombre", 14, cantaor);
+        boolean insertOk = consulta.insertCanciones(bd, "nombre", 14, cantaor);
+        if(!insertOk){
+            JOptionPane.showMessageDialog(vista, "Error al insertar la canción", "Error", JOptionPane.ERROR_MESSAGE);
+        }
         listaCanciones = consulta.mostrarCanciones(bd);
+        boolean updateOk = consulta.modificarCanciones(bd, listaCanciones.get(0), "Titulo nuevo", 10, cantaor);
+        if(!updateOk){
+            JOptionPane.showMessageDialog(vista, "Error al actualizar la canción", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        listaCanciones = consulta.mostrarCanciones(bd);
+        boolean deleteOk = consulta.borrarCanciones(bd, listaCanciones.get(0));
+        if(!deleteOk){
+            JOptionPane.showMessageDialog(vista, "Error al actualizar la canción", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
 }
--- src/Models/Consultas.java
+++ src/Models/Consultas.java
@@ -2,6 +2,7 @@ package Models;
 
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
+import com.db4o.ext.Db4oException;
 
 import java.util.ArrayList;
 
@@ -24,16 +25,64 @@ public class Consultas {
         return canciones;
     }
 
-    public void insertCanciones(ObjectContainer bd, String nombre, int duracion, Cantante cantante) {
-
+    public boolean insertCanciones(ObjectContainer bd, String nombre, int duracion, Cantante cantante) {
+        Canciones cancion = new Canciones(nombre, duracion);
+        cancion.setCantante(cantante);
+        // TODO: fix this duplicated code
+        //noinspection Duplicates
+        try {
+            bd.store(cancion);
+            bd.commit();
+            return true;
+        } catch (Db4oException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-    public void modificarCanciones(ObjectContainer bd) {
-
+    /**
+     * Recibe una cancion y los parametros para su modificación.
+     *
+     * @param bd            ObjectContainer
+     * @param cancion       Canciones
+     * @param nuevoTitulo   String
+     * @param nuevaDuracion int
+     * @param nuevoCantante Cantante
+     * @return boolean
+     */
+    public boolean modificarCanciones(ObjectContainer bd, Canciones cancion, String nuevoTitulo, int nuevaDuracion, Cantante nuevoCantante) {
+        Canciones cancionActualizar = (Canciones) bd.queryByExample(cancion).next();
+        cancionActualizar.setTitulo(nuevoTitulo);
+        cancionActualizar.setDuracion(nuevaDuracion);
+        cancionActualizar.setCantante(nuevoCantante);
+        // TODO: fix this duplicated code
+        //noinspection Duplicates
+        try {
+            bd.store(cancionActualizar);
+            bd.commit();
+            return true;
+        } catch (Db4oException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-    public void borrarCanciones(ObjectContainer bd) {
-
+    /**
+     * Borra una canción de la BBDD.
+     *
+     * @param bd      ObjectContainer
+     * @param cancion Canciones
+     * @return boolean
+     */
+    public boolean borrarCanciones(ObjectContainer bd, Canciones cancion) {
+        try {
+            bd.delete(bd.queryByExample(cancion));
+            bd.commit();
+            return true;
+        } catch (Db4oException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     /**
